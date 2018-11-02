b4a05d48c60b22a91ed23bc1e15a11321ca28aa6
Euldji Imane <43994254+EuldjiImane@users.noreply.github.com>
2018-11-02 11:17:00 +0100

    Add files via upload

---
+++ Facoders/Python/week4-2.py
@@ -0,0 +1,13 @@
+print("Numbers from 1 to 10")
+number=int(input("Guess the number: "))
+number1=4
+i=1
+while i<11:
+    if number ==number1:
+        print("Great! You did it!")
+        break
+    else:
+        print("Try again ")
+        number=int (input("Guess the number: "))
+
+    i=i+1
